6428b1384fdf4a1cbaeeebcd5f337746f3ec04f9
OwleyArts <facelessash@gmail.com>
2023-10-20 16:47:33 +0300

    VFX implemented

--- Assets/VFXStabConeControl.cs
+++ Assets/VFXStabConeControl.cs
@@ -11,16 +11,20 @@ public class VFXStabConeControl : MonoBehaviour, IConeControl
     public float Radius { get; set; }
     public float Angle { get; set; }
     private float coneRotationAngle;
+    [SerializeField] private ParticleSystem sparks;
+    [SerializeField] private GameObject cone;
 
     void OnEnable()
     {
         foreach (Material m in cone_materials)
         {
             m.SetFloat("_Cone", Angle);
+           
         }
-
+        cone.transform.localScale = new Vector3(Radius, 1, Radius);
         spears = this.GetComponent<ParticleSystem>();
         coneRotationAngle = -1 * (90 - (Angle / 2));
+        
     }
     // Update is called once per frame
     void Update()
@@ -28,10 +32,13 @@ public class VFXStabConeControl : MonoBehaviour, IConeControl
         var spearsAngle = spears.shape;
         var spearsAmmount = spears.emission.rateOverTime;
         var spearsRadius = spears.shape.radius;
+        var sparkPoint = sparks.shape.position;
 
         spearsAngle.arc = Angle % 360;
         spearsAngle.rotation = new Vector3(90f, coneRotationAngle, 0f);
-        spearsAmmount = Angle * 0.667f;
+        spearsAmmount = Angle * 0.75f;
         spearsRadius = Radius - 5f;
+        sparkPoint.z = Radius;
+        
     }
 }
