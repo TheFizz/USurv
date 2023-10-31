03e5f8bdebbc33299e831ab6e77aa9ac1033abeb
OwleyArts <facelessash@gmail.com>
2023-11-01 00:59:51 +0200

    StatusEffects VFX first batch
    -bleed
    -burn
    -prayer

---
+++ Assets/EnemyVFXAnchors.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVFXAnchors : MonoBehaviour
+{
+
+    public Transform[] handAnchor;
+    public Transform headAnchor;
+    public Transform groundAnchor;
+    public Transform chestAnchor;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+}
